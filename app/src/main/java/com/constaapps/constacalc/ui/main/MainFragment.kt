1874e45cf936d4994ce256b2ba8d99703f174c30
ConstantinCezB <Constantin.Cez.b@gmail.com>
2019-05-19 16:20:45 -0400

    Attached the grammar to the calculator and made it interact to the logic of the calculator.

--- app/src/main/java/com/constaapps/constacalc/ui/main/MainFragment.kt
+++ app/src/main/java/com/constaapps/constacalc/ui/main/MainFragment.kt
@@ -49,28 +49,50 @@ class MainFragment : Fragment() {
 
     private fun enableButtons(view: View) {
         val buttons = listOf<Button>(
-            view.button1, view.button2, view.button4, view.button5,
-            view.button6, view.button7, view.button8, view.button9, view.button10,
-            view.button11, view.button12, view.button13, view.button14, view.button15,
-            view.button16, view.button17, view.button18, view.button19
+            view.button1, view.button2, view.button5,
+            view.button6, view.button7, view.button9, view.button10,
+            view.button11, view.button13, view.button14, view.button15,
+            view.button17, view.button18
         )
-
-
 
         view.button20.let {
             it.setOnClickListener {
                 viewModel.currentFormula.value = viewModel.currentFormula.value!!.dropLast(1)
+                if (viewModel.currentFormula.value.toString().isBlank()){
+                    view.calculatorAnswer.text = "0.0"
+                }
             }
 
             it.setOnLongClickListener {
                 viewModel.currentFormula.value = ""
+                view.calculatorAnswer.text = "0.0"
                 return@setOnLongClickListener true
             }
-
         }
+
         view.button3.setOnClickListener {
             view.calculatorAnswer.text = CalculatorBrain.calculate(viewModel.currentFormula.value)
+        }
 
+        view.button4.setOnClickListener {
+            // This is the + button
+            viewModel.currentFormula.value = viewModel.currentFormula.value.orEmpty() + "+"
+        }
+        view.button8.setOnClickListener {
+            // This is the - button
+            viewModel.currentFormula.value = viewModel.currentFormula.value.orEmpty() + "-"
+        }
+        view.button12.setOnClickListener {
+            // This is the × button
+            viewModel.currentFormula.value = viewModel.currentFormula.value.orEmpty() + "*"
+        }
+        view.button16.setOnClickListener {
+            // This is the × button
+            viewModel.currentFormula.value = viewModel.currentFormula.value.orEmpty() + "/"
+        }
+        view.button19.setOnClickListener {
+            // This is the percentage (%) button
+            viewModel.currentFormula.value = viewModel.currentFormula.value.orEmpty() + "percentage"
         }
 
         buttons.forEach { button ->
