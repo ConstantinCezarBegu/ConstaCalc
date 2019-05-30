ad77c29de13c779eb36950e062c97f929dc017c0
ConstantinCezB <Constantin.Cez.b@gmail.com>
2019-05-29 22:59:31 -0400

    Created launch lauding time display.

---
+++ app/src/main/java/com/constaapps/constacalc/LaunchActivity.kt
@@ -0,0 +1,16 @@
+package com.constaapps.constacalc
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
